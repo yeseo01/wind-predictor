be9a57bd20ebd7881ec34f00ab20ef6da51321f0
Yeseo <yeseo011230@gmail.com>
2026-09-14 17:50:06 +0900

    refactor: improve model code readability

--- src/models/mlp.py
+++ src/models/mlp.py
@@ -1,30 +1,46 @@
+"""Multi-layer perceptron implemented from scratch with NumPy."""
+
+from __future__ import annotations
+
+import numpy as np
+
 from .layer import Layer
 
-class MultiLayerPerceptron():
-    def __init__(self, input_size, hidden_sizes, output_size):
-        self.layers = [] # 설계한 MLP에 존재하는 layer class들을 저장할 리스트
 
-        # 입력층
+class MultiLayerPerceptron:
+    """Feed-forward neural network composed of dense layers."""
+
+    def __init__(
+        self,
+        input_size: int,
+        hidden_sizes: list[int],
+        output_size: int,
+    ) -> None:
+        self.layers: list[Layer] = []
+
         self.layers.append(Layer(input_size, hidden_sizes[0]))
 
-        # 은닉층들
-        for i in range(len(hidden_sizes)-1):
-            self.layers.append(Layer(hidden_sizes[i], hidden_sizes[i+1]))
+        for i in range(len(hidden_sizes) - 1):
+            self.layers.append(
+                Layer(hidden_sizes[i], hidden_sizes[i + 1])
+            )
 
-        # 출력층
         self.layers.append(Layer(hidden_sizes[-1], output_size))
 
-    def forward(self, x):
-        # Forward
-        for layer in self.layers[:-1]:  # 마지막 레이어(출력층)를 제외한 모든 레이어
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        """Run a forward pass through the network."""
+        for layer in self.layers[:-1]:
             x = layer.forward(x)
             x = layer.relu(x)
 
-        # 출력층
         x = self.layers[-1].forward(x)
         return x
 
-    def backward(self, grad, learning_rate):
-        # Backward
-        for layer in reversed(self.layers):  # 출력층부터 입력층까지 역순으로
-            grad = layer.backward(grad, learning_rate) 
+    def backward(
+        self,
+        grad: np.ndarray,
+        learning_rate: float,
+    ) -> None:
+        """Backpropagate gradients through all layers."""
+        for layer in reversed(self.layers):
+            grad = layer.backward(grad, learning_rate)
